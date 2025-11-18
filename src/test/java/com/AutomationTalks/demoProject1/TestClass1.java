24fa30c97ef387f16d195ff12b509b9a1b4811f7
Rukhsana <rukhsana1409@gmail.com>
2025-11-18 12:29:39 -0500

    new

--- src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
+++ src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
@@ -1,62 +1,68 @@
 package com.AutomationTalks.demoProject1;
-import java.io.IOException;
-import java.net.MalformedURLException;
+
 import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
-
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class TestClass1 
-{
-	public static WebDriver driver;
-	
-	@BeforeMethod
-	public void launchDriver() throws IOException 
-	{
-		ChromeOptions options = new ChromeOptions();
-		
-		options.setPlatformName("WINDOWS");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guru\\Desktop\\Drivers\\chromedriver.exe");
-		 URL hubUrl = new URL("http://192.168.0.254:4444/wd/hub");
-		 driver = new RemoteWebDriver(hubUrl, options);
-			
-		//driver=new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.manage().window().fullscreen();
-		
-	}
-	@Test
-	public void Test1()
-	{
-		driver.navigate().to("https://www.AutomationTalks.com");
-		System.out.println("Test 1 Title is "+ driver.getTitle());
-	}
-	@Test
-	public void Test2()
-	{
-		driver.navigate().to("https://www.AutomationTalks.com");
-		System.out.println("Test 2 Title is "+ driver.getTitle());
-	}
-	@Test
-	public void Test3()
-	{
-		driver.navigate().to("https://www.AutomationTalks.com");
-		System.out.println("Test 3 Title is "+ driver.getTitle());
-	}
-	@AfterMethod
-	public void quit() {
-		driver.quit();
-	}
-	
+public class TestClass1 {
+    public static WebDriver driver;
+    public static final String USERNAME = "rukhsana_2Jz54B";
+    public static final String ACCESS_KEY = "S6CQpTuszscY2woqstsw";
+    public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    @BeforeMethod
+    public void launchDriver() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("browserName", "firefox");   // Proper casing, BrowserStack is case-insensitive but better to use standard
+        caps.setCapability("browserVersion", "latest");   // or "117.0"
+
+
+        Map<String, Object> bstackOptions = new HashMap<>();
+        bstackOptions.put("os", "Windows");
+        bstackOptions.put("osVersion", "11");
+        bstackOptions.put("sessionName", "My Selenium 4 Test");
+        bstackOptions.put("buildName", "Build 1");          // Optional but helps group sessions in dashboard
+        bstackOptions.put("seleniumVersion", "4.33.0");     // Optional but recommended
+        bstackOptions.put("video", true);                    // Enable video recording
+        caps.setCapability("bstack:options", bstackOptions);
+
+        driver = new RemoteWebDriver(new URL(URL), caps);
+        System.out.println("BrowserStack Session ID: " + ((RemoteWebDriver) driver).getSessionId());
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void Test1() {
+        driver.get("https://www.AutomationTalks.com");
+        System.out.println("Test 1 Title is " + driver.getTitle());
+    }
+
+    @Test
+    public void Test2() {
+        driver.get("https://www.AutomationTalks.com");
+        System.out.println("Test 2 Title is " + driver.getTitle());
+    }
+
+    @Test
+    public void Test3() {
+        driver.get("https://www.AutomationTalks.com");
+        System.out.println("Test 3 Title is " + driver.getTitle());
+    }
+
+    @AfterMethod
+    public void quit() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
-
-
